0aa31ea203dc6e49fb9416476360a2861ed0c2cf
EthanUnlocked690 <ethan.ioannou30@student.ccgs.nsw.edu.au>
2025-11-12 21:59:23 +1100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'theunlockeddash-v1';
+const FILES_TO_CACHE = [
+  'geometry-app.html',
+  'dash-icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
